4bca73d3319b6d892f8f2a1091eba880edd02293
Yao <GyonGyon@users.noreply.github.com>
2018-08-14 10:54:08 +0800

    update: change api path

--- src/config/apiTable.js
+++ src/config/apiTable.js
@@ -1,11 +1,10 @@
-
 const createApi = (path) => {
-  const base = '//47.97.171.140:8088/api/v1/'
+  const base = 'https://server.appchain.cryptape.com/api/v1/'
   return base + path
 }
 
 const createContentApi = (path) => {
-  const base = '//47.97.171.140:8088/api/v1/page_infos/'
+  const base = 'https://server.appchain.cryptape.com/api/v1/page_infos/'
   return base + path
 }
 
